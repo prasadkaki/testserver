96dd90da0953b7e15b1b09ff548f1df052581de0
prasadkaki <prasadkaki@rediffmail.com>
2019-12-31 00:21:33 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var handleRequest = function(request, response) {
+  response.writeHead(200);
+  response.end("Hello World! This is kaki");
+}
+var www = http.createServer(handleRequest);
+www.listen(8080);
